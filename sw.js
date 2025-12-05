3f9a17169176c8a3837de0f42b7be13319a9d2a4
jenerCruz <pamecoaxaca.telcdl@gmail.com>
2025-12-04 20:50:37 -0600

    commit

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "timelocus-cache-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./assets/css/tailwind.min.css",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
+];
+
+// Instalar SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("[SW] Cacheando app shell...");
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activar SW
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => k !== CACHE_NAME && caches.delete(k))
+      )
+    )
+  );
+});
+
+// Fetch offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).catch(() =>
+          caches.match("./index.html")
+        )
+      );
+    })
+  );
+});
